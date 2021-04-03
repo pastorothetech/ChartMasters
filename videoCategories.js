1fbf05160424a2c863293a0b5c8fecea59d36474
pastorothetech <81791387+pastorothetech@users.noreply.github.com>
2021-04-02 23:24:54 -0400

    testing

---
+++ videoCategories.js
@@ -0,0 +1,283 @@
+var categories = [
+"7 Continents",
+"Abraham",
+"Action",
+"Action / Sci-Fi",
+"Adam",
+"Adventure",
+"Aircrafts",
+"Anatomy",
+"Angels",
+"Animated Movies",
+"Animation",
+"Art",
+"Beauty and Fashion",
+"Biblical",
+"Biography",
+"Boxing",
+"Brass & Military",
+"Cartoon Movies",
+"Cartoons",
+"Children's",
+"Christmas",
+"Civil Rights",
+"Classical",
+"Comedy",
+"Concerts",
+"Countries",
+"Country",
+"Crime",
+"Culture",
+"Currency",
+"David",
+"Death",
+"Documentary",
+"Drama",
+"Electronic",
+"Entertainment",
+"Family",
+"Fantasy",
+"Fiction",
+"Film Noir",
+"Folk",
+"Folk, World, & Country",
+"Food",
+"Funerals",
+"Funk / Soul",
+"Galaxies",
+"Gaming",
+"Gospel",
+"Gospel Concerts",
+"Hip-Hop",
+"History",
+"Horror",
+"Indians",
+"International",
+"Isaac",
+"Ishmael",
+"Jacob",
+"Jazz",
+"Jesus",
+"Joseph",
+"Latin",
+"Laws",
+"Life",
+"Martial Arts",
+"Mathematics",
+"Military",
+"Moses",
+"Motivation",
+"Movies",
+"Music",
+"Musical",
+"Mystery",
+"Nature",
+"Nature",
+"Newage",
+"Non-Music",
+"North Korea",
+"Old Movies New",
+"Philosophy",
+"Physics",
+"Pop",
+"Pop/Rock",
+"Presidents",
+"Public Orbit Known & Unknown",
+"R&B",
+"Rap",
+"Reggae",
+"Religion",
+"Rock",
+"Romance",
+"Samuel",
+"Saul",
+"Science and Technology",
+"Sci-Fi",
+"Senate / Congress Hearing",
+"Senate Hearings",
+"Slavery",
+"Solomon",
+"Soundtrack",
+"Sport",
+"Sports",
+"Stage & Screen",
+"Superhero",
+"Talk Shows / Game Shows",
+"Talk Shows / Late Night",
+"Technology",
+"The Law",
+"Thriller",
+"Traditional",
+"Travel",
+"TV Shows",
+"Vehicles",
+"Video Games",
+"W.O.W",
+"War",
+"Wars",
+"Weather",
+"Western",
+"Western Shows",
+"Westerns",
+"World Changers",
+"WW2",
+"Black History"
+,
+"Terrorist Attacks "
+,
+"Faith Based Movies "
+,
+"Ministers/Preachers "
+,
+"Health/Medical "
+,
+"Black Movies "
+,
+"Disasters "
+,
+"Paranormal/Ghost/Demons "
+,
+"Weird and Strange "
+,
+"Talent Shows "
+,
+"DYI - Do it Yourself "
+,
+"Crime Shows "
+,
+"Massacres "
+,
+"Wrestling "
+,
+"Disease "
+,
+"Alien/UFO/USO "
+,
+"Racism "
+,
+"Spiritual "
+,
+"Gospel Music "
+,
+"Biblical/Christian Videos "
+,
+"Black Inventions "
+,
+"Comedy Movies "
+,
+"Creation "
+,
+"Evolution "
+,
+"UNDERGROUND RAILROAD "
+,
+"Black Movies "
+,
+"Stand-up Comedy "
+,
+"Comedy Movies "
+,
+"Ragga i "
+,
+"Jazz Music "
+,
+"Creation "
+,
+"Joshua "
+,
+"Bible Judges "
+,
+"COVID-19 "
+,
+"True Stories "
+,
+"People of Color killed by Police "
+,
+"Comedy Shows",
+"Miracles",
+"Miracles",
+"Media",
+"News Programs",
+"Elections",
+"Science",
+"TOWER OF BABEL",
+"SODOM & GOMORRAH",
+"NOAH",
+"Prophets of the Bible",
+"Seth",
+"Chart Master's Lesson Videos",
+"BLACK ON BLACK CRIME",
+"FISHING ",
+"DIY ",
+"Survival Videos",
+"911",
+"911 TERROR ATTACKS ",
+"Court"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
